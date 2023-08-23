f73a3f4512d0b9096f9231cee689b416f43b0b84
lihle-sibiya <129907260+lihle-sibiya@users.noreply.github.com>
2023-08-23 03:29:56 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
+import TodoApp from './TodoApp';
+
+class App extends React.Component {
+  render(){
+  return (
+    <TodoApp />
+  );
+  }
+}
+export default App;
